812ed58f2fd1fdda1a67524a62a33521b513b389
Olaf Mersmann <olafm@p-value.net>
2021-05-14 21:54:40 +0200

    Improve documentation.

--- R/kdpee.R
+++ R/kdpee.R
@@ -1,12 +1,17 @@
-#' D. Stowell and M. D. Plumbley
-#' Fast multidimensional entropy estimation by k-d partitioning.
-#' IEEE Signal Processing Letters 16 (6), 537--540, June 2009.
-#' http://dx.doi.org/10.1109/LSP.2009.2017346
+#' Fast entropy estimation of multi-dimensional data.
+#'
+#' Non-parametric estimator for the differential entropy of a multidimensional distribution, given a limited set of data points, by a recursive rectilinear partitioning.
 #'
 #' @param X data
 #' @param z Z-score threshold
 #'
-#' @return Entropy estimate
+#' @return Differential entropy estimate.
+#'
+#' @references
+#' D. Stowell and M. D. Plumbley
+#' Fast multidimensional entropy estimation by k-d partitioning.
+#' IEEE Signal Processing Letters 16 (6), 537--540, June 2009.
+#' http://dx.doi.org/10.1109/LSP.2009.2017346
 #'
 #' @examples
 #' Xu <- matrix(runif(1000 * 100), ncol=100)
